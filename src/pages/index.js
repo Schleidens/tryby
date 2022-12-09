1dc19b1c5bcf240fd64f9f255ccb5410adbe54f9
Schleidens <alectineschleidens2016@gmail.com>
2022-12-09 15:23:25 -0500

    fetch data and print

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,25 @@
+import { graphql } from "gatsby"
 import * as React from "react"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+
+  const { allContentfulTryby } = data;
+  const blogpost = allContentfulTryby.nodes;
+  console.log(blogpost);
   return (
     <>
-      Hello Tryby
+     <main>
+      {blogpost.map(blog => (
+        <div key={blog.id}>
+          <div>{blog.title}</div>
+          <div>{blog.date}</div>
+          {blog.tag.map(tag => (
+            <div>{tag}</div>
+          ))}
+          <img src={blog.image.url} alt="" />
+        </div>
+      ))}
+     </main>
     </>
   )
 }
@@ -11,3 +27,26 @@ const IndexPage = () => {
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
+
+export const blogpost = graphql`
+query MyQuery {
+  allFile {
+    nodes {
+      relativePath
+    }
+  }
+  allContentfulTryby {
+    nodes {
+      createdAt
+      date
+      tag
+      title
+      id
+      image {
+        id
+        url
+      }
+    }
+  }
+}
+`
