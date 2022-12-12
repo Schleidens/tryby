a10e6d99a8a53427defc41c512c2aa5e88579122
Schleidens <alectineschleidens2016@gmail.com>
2022-12-12 13:42:57 -0500

    create and design the header

---
+++ src/component/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div className='header'>
+        <h2>
+            This blog is a <span className='mark'>jamstack</span> demo project built with <span className='mark'>Gatsby/Contentful</span>.
+        </h2> 
+    </div>
+  )
+}
