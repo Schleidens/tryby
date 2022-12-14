c328e437f1088abbf3226e386be831af7f9de4d0
Schleidens <alectineschleidens2016@gmail.com>
2022-12-14 12:22:11 -0500

    work on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,33 @@ export default function about() {
   return (
     <div>
         <Layout>
-            About page
+            <div className="about">
+              <p>
+              <span className="mark">TRYBY </span> is a demo blog, built by <a href="https://github.com/Schleidens"><span className='mark'> Schleidens.Dev </span></a> using <span className="mark">Gatsby/Contentful</span>. 
+              </p>
+              <b>Techno</b>
+              <ul>
+                <li>
+                  <a href="https://www.gatsbyjs.com/">Gatsby</a>
+                </li>
+                <li>
+                  <a href="https://graphql.org/">GraphQl</a>
+                </li>
+                <li>
+                  <a href="https://www.contentful.com/">Contentful</a>
+                </li>
+                <li>
+                  <a href="https://www.gatsbyjs.com/products/cloud/">Gatsby Cloud</a>
+                </li>
+                <li>
+                  <a href="https://sass-lang.com/">Sass</a>
+                </li>
+              </ul>
+
+              <div className="">
+               // <em>This blog follows the jamstack architecture pattern, basically it consits of using a static site generator and a Headless CMS for backend (host and fetch data from)</em>
+              </div>
+            </div>
         </Layout>
     </div>
   )
